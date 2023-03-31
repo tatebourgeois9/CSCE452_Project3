661184f7c65a94f3a3eeae68efa9b38e62458d41
aaron5463737 <aaron54637@tamu.edu>
2023-03-31 01:45:44 -0500

    plz

--- project3_ws/src/project3_pkg/project3_pkg/laser_scan_to_pointcloud.py
+++ project3_ws/src/project3_pkg/project3_pkg/laser_scan_to_pointcloud.py
@@ -6,62 +6,86 @@ import math
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-
 class LaserScanToPointCloud(Node):
-
     def __init__(self):
         super().__init__('laser_scan_to_pointcloud')
         self.sub = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
         self.pub = self.create_publisher(PointCloud, '/scan_pointcloud', 10)
+        self.prev_points_history = []  # Store history of previous points
+        self.history_size = 50  # Number of previous point clouds to track
+        self.stationary_velocity_threshold = 30.0  # Threshold to consider a cluster's average velocity as stationary
+        self.cluster_eps = 0.5  # Maximum distance between two points to be considered in the same cluster
+        self.cluster_min_samples = 4  # Minimum number of points in a cluster
+        self.vicinity_threshold = 0.3  # Maximum distance between two points to be considered in the same vicinity
 
     def scan_callback(self, scan_msg):
-        # Convert LaserScan to PointCloud
         header = scan_msg.header
         points = []
+
         for i, r in enumerate(scan_msg.ranges):
-            if r != float('inf') and not math.isnan(r):  # Updated condition
-                # Calculate the angle of the ray
+            if r != float('inf') and r != float('nan'):
                 angle = scan_msg.angle_min + i * scan_msg.angle_increment
-                # Convert polar coordinates to Cartesian coordinates
                 x = r * math.cos(angle)
                 y = r * math.sin(angle)
                 z = 0.0
 
                 point = Point32(x=x, y=y, z=z)
-                # Add the point to the list of points
                 points.append(point)
-        cloud_msg = PointCloud(header=header, points=points)
 
-        # Filter out points that are not moving
-        filtered_points = []
-        for p in points:
-            if abs(p.x) > 0.3 or abs(p.y) > 0.3:
-                filtered_points.append(p)
-        filtered_cloud_msg = PointCloud(header=header, points=filtered_points)
+        self.prev_points_history.append(points)
 
-        # Cluster the filtered points using DBSCAN
-        if len(filtered_points) > 0:
-            X = np.array([[p.x, p.y] for p in filtered_points])
-            dbscan = DBSCAN(eps=0.5, min_samples=3)  # Customize eps and min_samples as needed
-            dbscan.fit(X)
-            labels = dbscan.labels_
-            unique_labels = np.unique(labels)
+        if len(self.prev_points_history) > self.history_size:
+            self.prev_points_history.pop(0)  # Remove the oldest point cloud
 
-            # Calculate the centroid for each cluster
-            centroids = []
-            for label in unique_labels:
-                if label != -1:
-                    cluster_points = X[labels == label]
-                    centroid = np.mean(cluster_points, axis=0)
-                    centroids.append(centroid)
-            centroid_points = [Point32(x=x, y=y, z=0.0) for x, y in centroids]
+        # Cluster the points using DBSCAN
+        X = np.array([[p.x, p.y] for p in points])
 
-            # Create PointCloud message for centroids
-            centroid_cloud_msg = PointCloud(header=header, points=centroid_points)
+        # Remove any NaN values from X
+        X = X[~np.isnan(X).any(axis=1)]
 
-            # Publish the centroid point cloud message
-            self.pub.publish(centroid_cloud_msg)
+        dbscan = DBSCAN(eps=self.cluster_eps, min_samples=self.cluster_min_samples)
+        dbscan.fit(X)
 
+        labels = dbscan.labels_
+        unique_labels = np.unique(labels)
+        moving_centroids = []
+
+        for label in unique_labels:
+            if label != -1:
+                cluster_points = X[labels == label]
+                cluster_velocities = []
+
+                # Calculate the velocities of each point in the cluster
+                for i, point in enumerate(cluster_points):
+                    if len(self.prev_points_history) >= 2:
+                        prev_point = self.prev_points_history[-2][i]
+                        dx = point[0] - prev_point.x
+                        dy = point[1] - prev_point.y
+                        dt = scan_msg.scan_time
+                        vx = dx / dt
+                        vy = dy / dt
+                        cluster_velocities.append((vx, vy))
+
+                # Calculate the average velocity of the cluster
+                if cluster_velocities:
+                    avg_velocity = np.mean(cluster_velocities, axis=0)
+                    magnitude = np.linalg.norm(avg_velocity)
+                    if magnitude > self.stationary_velocity_threshold:
+                        centroid = np.mean(cluster_points, axis=0)
+                        # Check if the centroid is close to any previously detected moving clusters
+                        is_close_to_previous = False
+                        for prev_centroid in moving_centroids:
+                            if np.linalg.norm(centroid - prev_centroid) < self.vicinity_threshold:
+                                is_close_to_previous = True
+                                break
+                        # Only add the centroid to the list of moving centroids if it is not close to any previous ones
+                        if not is_close_to_previous:
+                            moving_centroids.append(centroid)
+
+    # Publish the filtered point cloud containing only the centroids of the moving clusters
+        moving_centroid_points = [Point32(x=x, y=y, z=0.0) for x, y in moving_centroids]
+        moving_centroid_cloud_msg = PointCloud(header=header, points=moving_centroid_points)
+        self.pub.publish(moving_centroid_cloud_msg)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -70,11 +94,7 @@ def main(args=None):
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    node.destroy_node()
-    rclpy.shutdown()
+    node.destroy()
 
-
-if __name__ == '__main__':
-    main()
-
-
+main()
+   
