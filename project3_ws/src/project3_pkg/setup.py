8729b16d70194e0c1067ca1146f615287bbf8d2c
Chase Cason <chasecason@tamu.edu>
2023-03-30 21:39:26 -0500

    Working launcher

--- project3_ws/src/project3_pkg/setup.py
+++ project3_ws/src/project3_pkg/setup.py
@@ -11,8 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, glob.glob('launch/*')),
-        ('share/' + package_name, glob.glob('bags/*/*'))
+        ('share/' + package_name, glob.glob('launch/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
