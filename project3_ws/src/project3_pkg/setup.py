50396682333779a943f0ecbed12bdd5d87727035
Chase Cason <chasecason@tamu.edu>
2023-03-30 20:56:06 -0500

    Launch working with variables, bags accessible

--- project3_ws/src/project3_pkg/setup.py
+++ project3_ws/src/project3_pkg/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import glob
 
 package_name = 'project3_pkg'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob.glob('launch/*')),
+        ('share/' + package_name, glob.glob('bags/*/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
