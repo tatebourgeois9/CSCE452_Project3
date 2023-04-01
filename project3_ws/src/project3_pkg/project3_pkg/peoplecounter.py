34d5962a26b8f4207e8836104829508b0987953a
aaron5463737 <aaron54637@tamu.edu>
2023-03-31 19:37:02 -0500

    people

--- project3_ws/src/project3_pkg/project3_pkg/peoplecounter.py
+++ project3_ws/src/project3_pkg/project3_pkg/peoplecounter.py
@@ -1,44 +1,35 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point32
 import math
+import numpy as np
+from sklearn.cluster import DBSCAN
+
 class PeopleCounter(Node):
     def __init__(self):
         super().__init__('people_counter')
         self.sub = self.create_subscription(PointCloud, '/scan_pointcloud', self.pointcloud_callback, 10)
         self.total_unique_people = 0
         self.current_people = 0
-        self.last_people = []
-        self.matching_distance = 0.5
+        self.last_centroids = set()
 
     def pointcloud_callback(self, pointcloud_msg):
-        current_people = []
+        current_centroids = set()
         for point in pointcloud_msg.points:
-            current_people.append((point.x, point.y, point.z))
+            current_centroids.add((round(point.x, 3), round(point.y, 3), round(point.z, 3)))
 
-        # Match the current people with the last detected people
-        matched_indices = set()
-        for person in self.last_people:
-            min_distance = float('inf')
-            min_index = None
-            for i, current_person in enumerate(current_people):
-                if i in matched_indices:
-                    continue
-                distance = math.sqrt((current_person[0] - person[0])**2 + (current_person[1] - person[1])**2)
-                if distance < min_distance:
-                    min_distance = distance
-                    min_index = i
+        if not self.last_centroids:
+            self.total_unique_people += len(current_centroids)
+        else:
+            new_people = current_centroids - self.last_centroids
+            self.total_unique_people += len(new_people)
 
-            if min_distance < self.matching_distance:
-                matched_indices.add(min_index)
-
-        new_people_count = len(current_people) - len(matched_indices)
-        self.total_unique_people += new_people_count
-        self.current_people = len(current_people)
-        self.last_people = current_people
+        self.current_people = len(current_centroids)
+        self.last_centroids = current_centroids
 
         self.get_logger().info(f'Current people: {self.current_people}, Total unique people: {self.total_unique_people}')
-
+        print(f'Current people: {self.current_people}, Total unique people: {self.total_unique_people}')
     
 def main(args=None):
     rclpy.init(args=args)
